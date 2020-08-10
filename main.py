7a9b7c2e0438da11bd09fa96b887b81d0215b95f
Suraj Kumar <surajkumarnair07@gmail.com>
2020-08-10 06:07:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import turtle
+a = 5
+print(a)
+
+b = "hello"
+print(b
+)
+
+
+#square
+suraj_turtle = turtle.Turtle()
+
+def square1():
+  suraj_turtle.forward(100)
+  suraj_turtle.right(90)
+  suraj_turtle.forward(100)
+  suraj_turtle.right(90)
+  suraj_turtle.forward(100)
+  suraj_turtle.right(90)
+  suraj_turtle.forward(100)
+
+square1()
+
+def square2():
+  suraj_turtle.backward(200)
+  suraj_turtle.left(90)
+  suraj_turtle.forward(100)
+  suraj_turtle.right(90)
+  suraj_turtle.forward(100)
+  suraj_turtle.right(90)
+  suraj_turtle.forward(100)
+
+square2()
+
+suraj_turtle.forward(200)
+square1()
+
+
+square2()
+square1()
